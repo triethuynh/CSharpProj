f99c9d45fbc37da8beee68574aa4645d55ffa38b
BLUESKYLT\triet <triethuynh05@outlook.com>
2017-06-24 01:03:51 -0700

    Add my implementation for qotd 'Divisible rules without using % operator' (see 06.Programming Excercises - Page 3 - June 19 2017.pdf). C# implementation + test cases. Thanks.

---
+++ BasicContructSln/BasicContruct/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using BasicContructClassLibrary;
+
+/// <summary>
+/// Tests for the Basic Construct
+/// Triet Huynh @2017
+/// </summary>
+namespace BasicConstruct {
+	class Program {
+		const string PASS = "PASS";
+		const string FAIL = "FAIL";
+		const string YES = "Yes";
+		const string NO = "No";
+
+		static void Main(string[] args) {
+			Number dividend = new Number();
+			int count = 0;
+
+			Program P = new Program();
+			Console.WriteLine("** Is number a divisible by number b? **");
+			P.IsDivisibleOutput(number: null, divisor: 12, expected: false, testNumber: ++count);
+			P.IsDivisibleOutput(dividend, divisor: 0, expected: false, testNumber: ++count);
+			P.IsDivisibleOutput(dividend, divisor: 15, expected: true, testNumber: ++count);
+			
+			dividend.Dividend = 50;
+			P.IsDivisibleOutput(dividend, divisor: 1, expected: true, testNumber: ++count);
+			P.IsDivisibleOutput(dividend, divisor: 50, expected: true, testNumber: ++count);
+			P.IsDivisibleOutput(dividend, divisor: 5, expected: true, testNumber: ++count);
+			P.IsDivisibleOutput(dividend, divisor: 45, expected: false, testNumber: ++count);		
+			P.IsDivisibleOutput(dividend, divisor: 49, expected: false, testNumber: ++count);
+			P.IsDivisibleOutput(dividend, divisor: 51, expected: false, testNumber: ++count);
+			P.IsDivisibleOutput(dividend, divisor: 20, expected: false, testNumber: ++count);
+			P.IsDivisibleOutput(dividend, divisor: -1, expected: true, testNumber: ++count);
+			P.IsDivisibleOutput(dividend, divisor: -25, expected: true, testNumber: ++count);
+			P.IsDivisibleOutput(dividend, divisor: -30, expected: false, testNumber: ++count);
+			Console.WriteLine();
+
+			Console.WriteLine("** Is number a divisible by 2? **");
+			int[] numbers = { -100, 101, 102, -103, 104, 105, 106, 107, -108, 109, 110 };
+			for (int i = 0; i < numbers.Length; i++) {
+				dividend.Dividend = numbers[i];
+				P.IsDivisibleOutput(dividend, divisor: 2, expected: (numbers[i] % 2 == 0 ? true : false), testNumber: ++count);
+			}
+			Console.WriteLine();
+
+			Console.WriteLine("** Is number a divisible by 3? **");
+			int[] numbersDivisibleByThree = { -4011, -3, 0, 3, 9, 12, 99, 111, 10914};
+			for (int i = 0; i < numbersDivisibleByThree.Length; i++) {
+				dividend.Dividend = numbersDivisibleByThree[i];
+				P.IsDivisibleOutput(dividend, divisor: 3, expected: true, testNumber: ++count);
+			}
+			int[] numbersNotDivisibleByThree = { -4012, -7, -1, 8, 10, 11, 100, 10913 };
+			for (int i = 0; i < numbersNotDivisibleByThree.Length; i++) {
+				dividend.Dividend = numbersNotDivisibleByThree[i];
+				P.IsDivisibleOutput(dividend, divisor: 3, expected: false, testNumber: ++count);
+			}
+			Console.WriteLine();
+
+			Console.WriteLine("** Is number a divisible by 4? **");
+			int[] numbersDivisibleByFour = { -500, -496, -96, -12, -8, -4, 0, 4, 8, 12, 16, 20, 88, 100, 104, 144, 100064 };
+			for (int i = 0; i < numbersDivisibleByFour.Length; i++) {
+				dividend.Dividend = numbersDivisibleByFour[i];
+				P.IsDivisibleOutput(dividend, divisor: 4, expected: true, testNumber: ++count);
+			}
+			int[] numbersNotDivisibleByFour = { -590, -414, -87, -10, -7, 6, 11, 21, 99, 101, 510, 100094 };
+			for (int i = 0; i < numbersNotDivisibleByFour.Length; i++) {
+				dividend.Dividend = numbersNotDivisibleByFour[i];
+				P.IsDivisibleOutput(dividend, divisor: 4, expected: false, testNumber: ++count);
+			}
+			Console.WriteLine();
+
+			Console.WriteLine("** Is number a divisible by 5? **");
+			int[] numbersDivisibleByFive = { -500, -105, -100, -95, -15, -10, -5, 0, 5, 10, 15, 20, 25, 95, 100, 105, (Int32.MaxValue / 10) * 10 };
+			for (int i = 0; i < numbersDivisibleByFive.Length; i++) {
+				dividend.Dividend = numbersDivisibleByFive[i];
+				P.IsDivisibleOutput(dividend, divisor: 5, expected: true, testNumber: ++count);
+			}
+			int[] numberNotDivisibleByFive = { -599, -101, -6, 1, 9, 99, 101, 100051, (Int32.MaxValue / 10) * 10 - 1};
+			for (int i = 0; i < numberNotDivisibleByFive.Length; i++) {
+				dividend.Dividend = numberNotDivisibleByFive[i];
+				P.IsDivisibleOutput(dividend, divisor: 5, expected: false, testNumber: ++count);
+			}
+			Console.WriteLine();
+
+			Console.WriteLine("** Is number a divisible by 6? **");
+			int[] numbersDivisibleBySix = { -750, -120, -90, -30, -18, -12, -6, 0, 6, 12, 18, 24, 48, 120, 1038 };
+			int divisor = 6;
+			for (int i = 0; i < numbersDivisibleBySix.Length; i++) {
+				dividend.Dividend = numbersDivisibleBySix[i];
+				P.IsDivisibleOutput(dividend, divisor, expected: true, testNumber: ++count);
+			}
+			int[] numberNotDivisibleBySix = { -500, -303, -104, -20, -16, -15, -9, -2, 3, 8, 99, 100, 111 };
+			for (int i = 0; i < numberNotDivisibleBySix.Length; i++) {
+				dividend.Dividend = numberNotDivisibleBySix[i];
+				P.IsDivisibleOutput(dividend, divisor, expected: false, testNumber: ++count);
+			}
+			Console.WriteLine();
+
+			Console.WriteLine("** Is number a divisible by 7? **");
+			divisor = 7;
+			int[] numbersDivisibleBySeven = { -770, -133, -49, -28, -14, -7, 0, 7, 21, 35, 56, 70, 1024 * 7 };
+			for (int i = 0; i < numbersDivisibleBySeven.Length; i++) {
+				dividend.Dividend = numbersDivisibleBySeven[i];
+				P.IsDivisibleOutput(dividend, divisor, expected: true, testNumber: ++count);
+			}
+			int[] numberNotDivisibleBySeven = { -776, -132, -20, -13, -6, 3, 10, 78, 100, 1024 * 7 + 1 };
+			for (int i = 0; i < numberNotDivisibleBySeven.Length; i++) {
+				dividend.Dividend = numberNotDivisibleBySeven[i];
+				P.IsDivisibleOutput(dividend, divisor, expected: false, testNumber: ++count);
+			}
+			Console.WriteLine();
+
+			Console.WriteLine("** Is number a divisible by 8? **");
+			divisor = 8;
+			int[] numbersDivisibleByEight = { -880, -792, -104, -48, -16, -8, 0, 8, 24, 144, 976, 19013 * 8 };
+			for (int i = 0; i < numbersDivisibleByEight.Length; i++) {
+				dividend.Dividend = numbersDivisibleByEight[i];
+				P.IsDivisibleOutput(dividend, divisor, expected: true, testNumber: ++count);
+			}
+			int[] numberNotDivisibleByEight = { -1806, -404, -10 -2, 1, 9, 11, 25, 578, 19013 * 8 + 1 };
+			for (int i = 0; i < numberNotDivisibleByEight.Length; i++) {
+				dividend.Dividend = numberNotDivisibleByEight[i];
+				P.IsDivisibleOutput(dividend, divisor, expected: false, testNumber: ++count);
+			}
+			Console.WriteLine();
+
+			Console.WriteLine("** Is number a divisible by 9? **");
+			divisor = 9;
+			int[] numbersDivisibleByNine = { -10341, -999, -108, -45, -9, 0, 9, 18, 90, 954, 9675, 201701 * 9 };
+			for (int i = 0; i < numbersDivisibleByNine.Length; i++) {
+				dividend.Dividend = numbersDivisibleByNine[i];
+				P.IsDivisibleOutput(dividend, divisor, expected: true, testNumber: ++count);
+			}
+			int[] numberNotDivisibleByNine = { -10340, -101, -8, - 1, 19, 100, 201701 * 9 + 8 };
+			for (int i = 0; i < numberNotDivisibleByNine.Length; i++) {
+				dividend.Dividend = numberNotDivisibleByNine[i];
+				P.IsDivisibleOutput(dividend, divisor, expected: false, testNumber: ++count);
+			}
+			Console.WriteLine();
+
+			Console.WriteLine("** Is number a divisible by 11? **");
+			divisor = 11;
+			int[] numbersDivisibleByEleven = { -981904, -7260, -110, -44, -11, 0, 11, 66, 121, 7810, 10021, 980441};
+			for (int i = 0; i < numbersDivisibleByEleven.Length; i++) {
+				dividend.Dividend = numbersDivisibleByEleven[i];
+				P.IsDivisibleOutput(dividend, divisor, expected: true, testNumber: ++count);
+			}
+			int[] numberNotDivisibleByEleven = { -981905, -8260, -120, -42, -10, 1, 12, 98, 120, 7811, 10121, 981445 };
+			for (int i = 0; i < numberNotDivisibleByEleven.Length; i++) {
+				dividend.Dividend = numberNotDivisibleByEleven[i];
+				P.IsDivisibleOutput(dividend, divisor, expected: false, testNumber: ++count);
+			}
+			Console.WriteLine();
+
+			Console.ReadLine();
+		}
+
+		/// <summary>
+		/// Output onto the console if the number is divisible by the divisor
+		/// </summary>
+		/// <param name="number">the instance of Number</param>
+		/// <param name="divisor">the divisor</param>
+		/// <param name="expected">the expected test result</param>
+		/// <param name="testNumber">The test case number</param>
+		public void IsDivisibleOutput(Number number, int divisor, bool expected, int testNumber) {
+			if (number == null) {
+				Console.WriteLine(testNumber + ". Invalid input: Number can't be null.");
+				return;
+			}
+
+			try {
+				bool actual = number.isDivisibleBy(divisor);
+				string actualResultText = (actual ? YES : NO);
+				string testResult = (actual == expected? PASS : FAIL);
+				Console.WriteLine(string.Format("{0}. [{1}] Is {2} divisible by {3}? {4}", testNumber, testResult, number.Dividend, divisor, actualResultText));
+			} catch (DivideByZeroException ex) {
+				Console.WriteLine(testNumber + ". " + ex.Message);
+			} catch (Exception ex) {
+				Console.WriteLine(testNumber + ". " + ex.Message);
+			}
+		}
+	}
+
+}
---
+++ BasicContructSln/BasicContructClassLibrary/BasicConstruct.cs
@@ -0,0 +1,292 @@
+﻿using System;
+
+/// <summary>
+/// Problem statement: SDE Skill - qotd 06/23/2017
+/// Triet Huynh @2017
+/// Ref.: Divisible rules
+///		1. Problem statement in "06.Programming Excercises - Page 3 - June 19 2017.pdf" from "qotd" of "SDE Skills" slack channel
+///		2. http://123doc.org/document/557091-dau-hieu-chia-het-cho-2-3-4-5-6-7-8-9.htm (Vietnamese)
+/// </summary>
+namespace BasicContructClassLibrary {
+	/// <summary>
+	/// Class DivisibilityRules
+	/// </summary>
+	public class Number {
+		private int number;
+
+		/// <summary>
+		/// Default contructor
+		/// </summary>
+		public Number() {
+			this.number = 0;
+		}
+
+		/// <summary>
+		/// One argument constructor, the dividend
+		/// </summary>
+		/// <param name="number">the dividend</param>
+		public Number(int number) {
+			this.number = number;
+		}
+
+		/// <summary>
+		/// Accessor for the number
+		/// </summary>
+		public int Dividend {
+			get { return this.number; }
+			set { this.number = value; }
+		}
+
+		/// <summary>
+		/// Determine if the dividend (number) is divisible by the given divisor
+		/// </summary>
+		/// <param name="divisor"></param>
+		/// <returns>true if the dividend (number) is divisible by the given divisor</returns>
+		public bool isDivisibleBy(int divisor) {
+			try {
+				return this.isDivisible(this.number, divisor);
+			} catch (Exception ex) {
+				throw ex;
+			}
+		}
+
+		/// <summary>
+		/// Determine if the given dividend is divisible by the given divisor
+		/// </summary>
+		/// <param name="dividend">the given dividend</param>
+		/// <param name="divisor">the given divisor</param>
+		/// <returns>true if the given dividend is divisible by the given divisor; false, otherwise</returns>
+		public bool isDivisible(int dividend, int divisor) {
+			if (divisor == 0) {
+				throw new DivideByZeroException();
+			}
+
+			int quotient = dividend / divisor;
+			int remainder = dividend - quotient * divisor;
+
+			return (remainder == 0 ? true : false);
+		}
+
+		/// <summary>
+		/// Accessor to determine if the number is divisible by two (2).
+		/// A number is divisible by 2 if the last digit is divisible by 2
+		/// </summary>
+		public bool IsDivisibleByTwo {
+			get {
+				int lastDigit = this.LastDigit;
+
+				return (lastDigit == 0 || lastDigit == 2 || lastDigit == 4 || lastDigit == 6 || lastDigit == 8);
+			}
+		}
+
+		/// <summary>
+		/// Accessor to determine if the number is divisible by three (3).
+		/// A number is divisible by 3 if the sum of all digits is divisible by 3
+		/// </summary>
+		public bool IsDivisibleByThree {
+			get {
+				if (this.number == 0 || this.number == 3) {
+					return true;
+				}
+
+				int sum = this.LastDigit;
+
+				int quotient = this.number / 10;
+				while (quotient > 9) {
+					sum += this.GetLastDigit(quotient);
+					quotient /= 10;
+				}
+
+				if (this.number > 9) {
+					sum += quotient;
+				}
+
+				return this.isDivisible(dividend: sum, divisor: 3);
+			}
+		}
+
+		/// <summary>
+		/// Accessor to determine if the number is divisible by four (4).
+		/// A number is divisible by 4 if the last two digits formed a number which is divisible by 4
+		/// </summary>
+		public bool IsDivisibleByFour {
+			get {
+				// One digit numbers
+				int absOfNumber = Math.Abs(number);
+				const int four = 4;
+				if (absOfNumber < 10) {
+					return (absOfNumber == 0 || absOfNumber == 4 || absOfNumber == 8);
+				}
+
+				// Two-digit numbers
+				if (absOfNumber < 100) {
+					return this.isDivisibleBy(divisor: four);
+				}
+
+				// Numbers with more than two-digits
+				int rightMostDigit = this.GetLastDigit(number: absOfNumber);
+				int tenthDigit = this.GetLastDigit(absOfNumber / 10);
+
+				return this.isDivisible(dividend: (tenthDigit * 10 + rightMostDigit), divisor: four);
+			}
+		}
+
+		/// <summary>
+		/// Accessor to determine if the number is divisible by five (5).
+		/// A number is divisible by 5 if the last digit is divisible by 5
+		/// </summary>
+		public bool IsDivisibleByFive {
+			get {
+				int rightMostDigit = this.GetLastDigit(this.number);
+
+				return this.isDivisible(dividend: rightMostDigit, divisor: 5);
+			}
+		}
+
+		/// <summary>
+		/// Accessor to determine if the number is divisible by six (6).
+		/// A number is divisible by 6 if it is divisible by 2 and by 3
+		/// </summary>
+		public bool IsDivisibleBySix {
+			get {
+				return this.isDivisible(dividend: this.number, divisor: 2) && 
+						this.isDivisible(dividend: this.number, divisor: 3);
+			}
+		}
+
+		/// <summary>
+		/// Accessor to determine if the number is divisible by seven (7).
+		/// A number is divisible by 7 if it is divisible by 7 if the following condition is true:
+		/// Represent the number as 10A + B.
+		/// If (A - 2B) is divisible by 7, then 10A + B is also divisible by 7
+		/// </summary>
+		public bool IsDivisibleBySeven {
+			get {
+				int absOfNumber = Math.Abs(number);
+				// One-digit dividend
+				if (absOfNumber < 10) {
+					return (absOfNumber == 0 || absOfNumber == 7);
+				}
+
+				// Two-or-more digit dividend
+				// B: Right most digit
+				int rightMostDigit = this.GetLastDigit(this.number);
+
+				// A: Remainder
+				int remainder = this.number / 10;
+
+				// dividend: A - 2B
+				return this.isDivisible(dividend: remainder - (rightMostDigit >> 1), divisor: 7);
+			}
+		}
+
+		/// <summary>
+		/// Accessor to determine if the number is divisible by eight (8).
+		/// A number is divisible by 8 if the last three digits formed a number which is divisible by 8
+		/// </summary>
+		public bool IsDivisibleByEight {
+			get {
+				// One digit dividends
+				int absOfNumber = Math.Abs(number);
+				const int eight = 8;
+				if (absOfNumber < 10) {
+					return (absOfNumber == 0 || absOfNumber == 8);
+				}
+
+				// Two-digit numbers divisible by 8:
+				// 0, 8, 16, 24, 32, 40, 48, 56, 64, 72, 80, 88, 96
+				if (absOfNumber < 100) {
+					// Use the general rule
+					return this.isDivisible(dividend: this.number, divisor: eight);
+				}
+
+				// Numbers with more than two-digits
+				int rightMostDigit = this.GetLastDigit(number: absOfNumber);
+				int tenthDigit = this.GetLastDigit(absOfNumber / 10);
+				int hundredthDigit = this.GetLastDigit(absOfNumber / 100);
+
+				return this.isDivisible(dividend: (hundredthDigit * 100 + tenthDigit * 10 + rightMostDigit), divisor: eight);
+			}
+		}
+
+		/// <summary>
+		/// Accessor to determine if the number is divisible by nine (9).
+		/// A number is divisible by 9 if the sum of all digits is divisible by 9
+		/// </summary>
+		public bool IsDivisibleByNine {
+			get {
+				int absOfNumber = Math.Abs(number);
+				const int divisor = 9;
+
+				int sum = this.GetLastDigit(number: absOfNumber);
+				int quotient = this.number / divisor;
+				while (quotient > 9) {
+					sum += this.GetLastDigit(number: quotient);
+					quotient /= divisor;
+				}
+				
+				if (this.number > 10) {
+					sum += quotient;
+				}
+
+				return this.isDivisible(dividend: sum, divisor: divisor);
+			}
+		}
+
+		/// <summary>
+		/// Accessor to determine if the number is divisible by eleven (11).
+		/// A number is divisible by 11 if the difference between the sum of all even-position digits and all the odd-position digits is divisible by 11
+		/// </summary>
+		public bool IsDivisibleByEleven {
+			get {
+				int absOfNumber = Math.Abs(number);
+				const int divisor = 11;
+
+				int sumOfEvenPositionDigits = this.GetLastDigit(number: absOfNumber);
+				int sumOfOddPositionDigits = 0;
+				int quotient = this.number / 10;
+				bool isEvenPosition = true;
+				while (quotient > 9) {
+					if (isEvenPosition) { // even positions
+						sumOfEvenPositionDigits += this.GetLastDigit(number: quotient);
+						isEvenPosition = false;
+					} else {
+						sumOfOddPositionDigits += this.GetLastDigit(number: quotient);
+						isEvenPosition = true;
+					}
+					quotient /= 10;
+				}
+
+				if (this.number > 10) {
+					if (isEvenPosition) { // even positions
+						sumOfEvenPositionDigits += quotient;
+					} else {
+						sumOfOddPositionDigits += quotient;
+					}
+				}
+
+				return this.isDivisible(dividend: sumOfEvenPositionDigits - sumOfOddPositionDigits, divisor: divisor);
+			}
+		}
+
+		#region private accessors, helper methods
+		/// <summary>
+		/// Accessor to obtain the last digit of the number (this.number)
+		/// </summary>
+		/// <returns>The last digit</returns>
+		private int LastDigit {
+			get {
+				return this.GetLastDigit(this.number);
+			}
+		}
+
+		/// <summary>
+		/// Obtain the last digit of the given number
+		/// <<param name="number">the given number</param>
+		private int GetLastDigit(int number) {
+				const int divisor = 10;
+				return this.number - (this.number / divisor) * divisor;
+		}
+		#endregion private helper methods
+	}
+}
